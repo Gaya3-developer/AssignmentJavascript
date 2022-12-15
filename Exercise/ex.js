9c6335247112b0e90282db413f142c02c6937476
Gaya3-developer <gayathrikbhat9@gmail.com>
2022-12-15 15:40:35 +0530

    new assignment

---
+++ Exercise/ex.js
@@ -0,0 +1,18 @@
+const arr=[1,2,3,4];
+const mapArray = arr.map( (element) => 
+    element * 2
+     
+
+)
+console.log(mapArray)
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(item => item * 2);
+console.log(doubled); // [2, 4, 6, 8]
+
+
+const obj = {
+    firstName : "hello"
+}
+obj.lastname = "world";
+console.log(obj)
